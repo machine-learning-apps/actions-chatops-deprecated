c4bf16f63395c06577e5005d684b0c8eedbb6abc
Hamel Husain <hamel.husain@gmail.com>
2019-09-24 10:33:58 -0700

    fix script

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,8 +24,7 @@ ls $GITHUB_EVENT_PATH
 # skip if trigger phase does not exist
 echo "Checking if comment contains '${INPUT_TRIGGER_PHRASE}' command..."
 COMMENT_BODY=$(jq -r ".comment.body" "$GITHUB_EVENT_PATH")
-echo "COMMENT BODY:"
-echo $COMMENT_BODY
+echo "COMMENT BODY: $COMMENT_BODY"
 (echo $COMMENT_BODY | grep -q "${INPUT_TRIGGER_PHRASE}") || no_trigger
 
 # skip if not a PR
